fb6903fa517dad574274e86dd7ade774aa0f95cc
Lubomir <100048089+lubomir80@users.noreply.github.com>
2025-05-22 12:05:37 +0200

    Menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { ShoppingCart, UserIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
-import ModeToggle from './mode'
+import Menu from './menu'
 
 
 
@@ -25,21 +23,7 @@ function Header() {
                   </span>
                </Link>
             </div>
-            <div className="space-x-2 ">
-               <ModeToggle />
-               <Button asChild variant="ghost">
-                  <Link href="/cart" >
-                     <ShoppingCart />
-                     Cart
-                  </Link>
-               </Button>
-               <Button asChild>
-                  <Link href="/sign-in" >
-                     <UserIcon />
-                     Sign-in
-                  </Link>
-               </Button>
-            </div>
+            <Menu />
          </div>
       </header>
    )
---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button"
+import ModeToggle from "./mode"
+import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react"
+import Link from "next/link"
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
+
+
+function Menu() {
+   return (
+      <div className="flex justify-end gap-3">
+         <nav className="hidden md:flex w-full max-w-sm gap-1 ">
+            <ModeToggle />
+            <Button asChild variant="ghost">
+               <Link href="/cart" >
+                  <ShoppingCart />
+                  Cart
+               </Link>
+            </Button>
+            <Button asChild>
+               <Link href="/sign-in" >
+                  <UserIcon />
+                  Sign-in
+               </Link>
+            </Button>
+         </nav>
+         <nav className="md:hidden">
+            <Sheet>
+               <SheetTrigger className="align-middle">
+                  <EllipsisVertical />
+               </SheetTrigger>
+               <SheetContent className="p-5 flex flex-col items-start ">
+                  <SheetTitle>Menu</SheetTitle>
+                  <ModeToggle />
+                  <Button asChild variant="ghost">
+                     <Link href="/cart" >
+                        <ShoppingCart />
+                        Cart
+                     </Link>
+                  </Button>
+                  <Button asChild>
+                     <Link href="/sign-in" >
+                        <UserIcon />
+                        Sign-in
+                     </Link>
+                  </Button>
+               </SheetContent>
+            </Sheet>
+         </nav>
+      </div>
+   )
+}
+
+export default Menu
